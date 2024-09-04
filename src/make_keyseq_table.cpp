9d493d18d28f0154f3997ea4ec97717f4fd0f24b
al-martyn1 <amart@mail.ru>
2024-09-05 02:02:41 +0300

    Размышляем

--- src/make_keyseq_table.cpp
+++ src/make_keyseq_table.cpp
@@ -135,13 +135,15 @@ int main(int argc, char* argv[])
 
 #if 0
 
+
+
 THardwareInfo::getTickCount();
 
 system.h
 
 struct TEvent
 {
-    ushort what;
+    ushort what;  // ev.what = evKeyDown;
     union
     {
         MouseEventType mouse;
@@ -158,9 +160,9 @@ struct TEvent
 
 struct CharScanType
 {
-#if !defined( TV_BIG_ENDIAN )
-    uchar charCode;
-    uchar scanCode;
+#if !defined( TV_BIG_ENDIAN ) // LITTLE_ENDIAN
+    uchar charCode;             // KeyEvent.uChar.AsciiChar            - ASCII код клавиши - младшая часть uint16
+    uchar scanCode;             // KEY_EVENT_RECORD.wVirtualScanCode;  - скан-код
 #else
     // Due to the reverse byte order, swap the fields in order to preserve
     // the aliasing with KeyDownEvent::keyCode.
@@ -169,6 +171,94 @@ struct CharScanType
 #endif
 };
 
+/* Turbo Vision stores key events in a KeyDownEvent struct, defined in
+ * system.h. Its first field is a key code (16 bit), which can be decomposed
+ * into the ASCII equivalent (lower byte) and a scan code (higher byte).
+ * It has a second field with the state of the modifier keys, which can be
+ * retrieved by performing a bit-wise AND with the kbShift, kbCtrlShift and
+ * kbAltShift bitmasks. Turbo Vision expects this field to be filled even
+ * if the key code is already named Shift/Ctrl/Alt+something. */
+
+/* The support for key combinations is the following:
+   - PrintScreen, Break are not likely to be captured by the terminal, but
+     Ctrl+C could be used as a replacement of the Ctrl+Break interrupt.
+   - Ctrl/Alt+F(n) don't work on the linux console and I strongly advice against
+     using them.
+   - Ctrl+Letter works, except for ^H, ^I, ^J and ^M, which have a special
+     meaning.
+   - Alt+Letter/Number seem to work quite well.
+   - Ctrl+Backspace/Enter can't be recognized on terminal emulators.
+   - Shift/Ctrl+Ins/Del/Home/End/PgDn/PgUp seem to work, too.
+   - Arrow keys work, as well as combined with Shift, but Turbo Vision doesn't
+     support Ctrl+Up/Down (EDIT: it now does).
+   - Tab and Backtab are supported too, although the linux console confuses the
+     latter with Alt+Tab.
+   - Some other key combinations are supported on terminal but not in Turbo Vision.
+ * Still, it's up to your luck that ncurses manages to grab any of these
+ * combinations from your terminal application. */
+
+Скан-код - https://ru.wikipedia.org/wiki/%D0%A1%D0%BA%D0%B0%D0%BD-%D0%BA%D0%BE%D0%B4
+https://snipp.ru/handbk/scan-codes
+
+tkeys.h
+
+ncursinp.cpp - таблицы 
+static constexpr KeyDownEvent fromNonPrintableAscii[32]
+static const const_unordered_map<ushort, KeyDownEvent> fromCursesKeyCode =
+
+far2l.cpp
+static const const_unordered_map<uchar, ushort> virtualKeyCodeToKeyCode =
+static const const_unordered_map<uchar, uchar> virtualKeyCodeToScanCode =
+
+terminal.cpp
+static const const_unordered_map<ushort, constarray<ushort, 3>> moddedKeyCodes =
+
+
+__FLAT__ - определено для любых 32+ как я понял, и не определено для 16 битных программ
+
+compat/windows/windows.h
+
+#define VK_SHIFT          0x10
+#define VK_CONTROL        0x11
+#define VK_MENU           0x12
+
+#define VK_LSHIFT         0xA0
+#define VK_RSHIFT         0xA1
+#define VK_LCONTROL       0xA2
+#define VK_RCONTROL       0xA3
+#define VK_LMENU          0xA4
+#define VK_RMENU          0xA5
+
+
+
+#define RIGHT_ALT_PRESSED     0x0001 // the right alt key is pressed.
+#define LEFT_ALT_PRESSED      0x0002 // the left alt key is pressed.
+#define RIGHT_CTRL_PRESSED    0x0004 // the right ctrl key is pressed.
+#define LEFT_CTRL_PRESSED     0x0008 // the left ctrl key is pressed.
+#define SHIFT_PRESSED         0x0010 // the shift key is pressed.
+#define NUMLOCK_ON            0x0020 // the numlock light is on.
+#define SCROLLLOCK_ON         0x0040 // the scrolllock light is on.
+#define CAPSLOCK_ON           0x0080 // the capslock light is on.
+#define ENHANCED_KEY          0x0100 // the key is enhanced.
+
+
+    kbLeftShift   = SHIFT_PRESSED,
+    kbRightShift  = SHIFT_PRESSED,
+    kbShift       = kbLeftShift | kbRightShift,
+    kbLeftCtrl    = LEFT_CTRL_PRESSED,
+    kbRightCtrl   = RIGHT_CTRL_PRESSED,
+    kbCtrlShift   = kbLeftCtrl | kbRightCtrl,
+    kbLeftAlt     = LEFT_ALT_PRESSED,
+    kbRightAlt    = RIGHT_ALT_PRESSED,
+    kbAltShift    = kbLeftAlt | kbRightAlt,
+    kbScrollState = SCROLLLOCK_ON,
+    kbNumState    = NUMLOCK_ON,
+    kbCapsState   = CAPSLOCK_ON,
+    kbEnhanced    = ENHANCED_KEY,
+    kbInsState    = 0x200,  // Ensure this doesn't overlap above values
+    kbPaste       = 0x400;
+
+
 struct KeyDownEvent
 {
     union
@@ -176,13 +266,95 @@ struct KeyDownEvent
         ushort keyCode;
         CharScanType charScan;
         };
-    ushort controlKeyState;
+    ushort controlKeyState;     // KeyEvent.dwControlKeyState & (kbShift | kbCtrlShift | kbAltShift | kbScrollState | kbNumState | kbCapsState | kbEnhanced );
     char text[4];               // NOT null-terminated.
     uchar textLength;
 
     TStringView getText() const;
     operator TKey() const;
 };
+
+
+kbCtrlZ     = 0x001a
+
+        if (res & (1 << KG_SHIFT))
+            actualModifiers |= kbShift;
+        if (res & (1 << KG_CTRL))
+            actualModifiers |= kbLeftCtrl;
+        if (res & (1 << KG_ALT))
+            actualModifiers |= kbLeftAlt;
+
+
+bool getWin32Key(const KEY_EVENT_RECORD &KeyEvent, TEvent &ev, InputState &state) noexcept
+{
+    if (!getWin32KeyText(KeyEvent, ev, state))
+        return false;
+
+    ev.what = evKeyDown;
+    ev.keyDown.charScan.scanCode = KeyEvent.wVirtualScanCode;
+    ev.keyDown.charScan.charCode = KeyEvent.uChar.AsciiChar;
+    ev.keyDown.controlKeyState = KeyEvent.dwControlKeyState & (
+        kbShift | kbCtrlShift | kbAltShift |
+        kbScrollState | kbNumState | kbCapsState | kbEnhanced
+    );
+
+    if (ev.keyDown.textLength)
+    {
+        ev.keyDown.charScan.charCode = CpTranslator::fromUtf8(ev.keyDown.getText());
+        if (KeyEvent.wVirtualKeyCode == VK_MENU)
+            // This is enabled when pasting certain characters, and it confuses
+            // applications. Clear it.
+            ev.keyDown.charScan.scanCode = 0;
+        if (!ev.keyDown.charScan.charCode || ev.keyDown.keyCode <= kbCtrlZ)
+            // If the character cannot be represented in the current codepage,
+            // or if it would accidentally trigger a Ctrl+Key combination,
+            // make the whole keyCode zero to avoid side effects.
+            ev.keyDown.keyCode = kbNoKey;
+    }
+
+    if ( ev.keyDown.keyCode == 0x2A00 || ev.keyDown.keyCode == 0x1D00 ||
+         ev.keyDown.keyCode == 0x3600 || ev.keyDown.keyCode == 0x3800 ||
+         ev.keyDown.keyCode == 0x3A00 )
+        // Discard standalone Shift, Ctrl, Alt, Caps Lock keys.
+        ev.keyDown.keyCode = kbNoKey;
+    else if ( (ev.keyDown.controlKeyState & kbCtrlShift) &&
+              (ev.keyDown.controlKeyState & kbAltShift) ) // Ctrl+Alt is AltGr.
+    {
+        // When AltGr+Key does not produce a character, a
+        // keyCode with unwanted effects may be read instead.
+        if (!ev.keyDown.textLength)
+            ev.keyDown.keyCode = kbNoKey;
+    }
+    else if (KeyEvent.wVirtualScanCode < 89)
+    {
+        // Convert NT style virtual scan codes to PC BIOS codes.
+        uchar index = KeyEvent.wVirtualScanCode;
+        ushort keyCode = 0;
+        if ((ev.keyDown.controlKeyState & kbAltShift) && THardwareInfo::AltCvt[index])
+            keyCode = THardwareInfo::AltCvt[index];
+        else if ((ev.keyDown.controlKeyState & kbCtrlShift) && THardwareInfo::CtrlCvt[index])
+            keyCode = THardwareInfo::CtrlCvt[index];
+        else if ((ev.keyDown.controlKeyState & kbShift) && THardwareInfo::ShiftCvt[index])
+            keyCode = THardwareInfo::ShiftCvt[index];
+        else if ( !(ev.keyDown.controlKeyState & (kbShift | kbCtrlShift | kbAltShift)) &&
+                  THardwareInfo::NormalCvt[index] )
+            keyCode = THardwareInfo::NormalCvt[index];
+
+        if (keyCode != 0)
+        {
+            ev.keyDown.keyCode = keyCode;
+            if (ev.keyDown.charScan.charCode < ' ')
+                ev.keyDown.textLength = 0;
+            else if (ev.keyDown.charScan.charCode < 0x7F && !ev.keyDown.textLength)
+            {
+                ev.keyDown.text[0] = ev.keyDown.charScan.charCode;
+                ev.keyDown.textLength = 1;
+            }
+        }
+    }
+
+    return ev.keyDown.keyCode != kbNoKey || ev.keyDown.textLength;
+}
 
 
 
