b25cc634ae741f507a06cc959f0d4f391fef0826
Alexandr Martynov <mag@unicont.com>
2024-09-08 17:02:26 +0300

    Копирование зависимостей sfmt в рабочий проект

--- hcp_sfmt.bat
+++ hcp_sfmt.bat
@@ -1,4 +1,6 @@
-@set OPTS=-U -I%~dp0_libs -oC:\work\projects\firmware\firmware_ES_4007_BMCL\_libs\umba\
+@set OPTS=-U -I%~dp0_libs -I%~dp0_libs\sfmt\inc -oC:\work\projects\firmware\firmware_ES_4007_BMCL\_libs\umba\
 umba-hcp %OPTS% %~dp0_libs\sfmt\inc\umba\simple_formatter.h
+
+
 
 
